1f694cf93d6b4ae41b9e307a5a3345aa35d456f3
Timmy Adeyeloja <adefeyitimi@gmail.com>
2025-06-05 09:49:32 +0100

    fix: removed translation

--- apps/cowswap-frontend/src/i18n.tsx
+++ apps/cowswap-frontend/src/i18n.tsx
@@ -1,29 +1,7 @@
-import { ReactNode, useCallback } from 'react'
+import { ReactNode } from 'react'
 
-import { SupportedLocale } from '@cowprotocol/common-const'
-
-import { initialLocale, useActiveLocale } from 'legacy/hooks/useActiveLocale'
-import { useUserLocaleManager } from 'legacy/state/user/hooks'
-
-import { dynamicActivate, Provider } from 'lib/i18n'
-
-dynamicActivate(initialLocale)
+import { Provider } from 'lib/i18n'
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
-  const locale = useActiveLocale()
-  const [, setUserLocale] = useUserLocaleManager()
-
-  const onActivate = useCallback(
-    (locale: SupportedLocale) => {
-      document.documentElement.setAttribute('lang', locale)
-      setUserLocale(locale) // stores the selected locale to persist across sessions
-    },
-    [setUserLocale]
-  )
-
-  return (
-    <Provider locale={locale} onActivate={onActivate}>
-      {children}
-    </Provider>
-  )
+  return <Provider>{children}</Provider>
 }
